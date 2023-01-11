4ef898052387a91c171c495e51151375776d28b2
Derry Renaldy <derryrenaldy@ayoconnect.id>
2023-01-11 10:37:52 +0700

    fix: make if condition

--- lib/main.dart
+++ lib/main.dart
@@ -14,34 +14,34 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  final questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionText': 'What\'s your favorite food?',
+      'answers': ['Noodle', 'Meatball', 'Spagheti', 'Steak'],
+    }
+  ];
 
   void _answerQuestion() {
-    setState(() {
-      _questionIndex += 1;
-      if (_questionIndex > 2){
-        _questionIndex = 0;
-      };
-    });
+    if (_questionIndex < questions.length-1) {
+      print('We have more questions!');
+      setState(() {
+        _questionIndex += 1;
+      });
+    }
+    print(_questionIndex);
   }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    print(_questionIndex);
-    var questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['Black', 'Red', 'Green', 'White'],
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
-      },
-      {
-        'questionText': 'What\'s your favorite food?',
-        'answers': ['Noodle', 'Meatball', 'Spagheti', 'Steak'],
-      }
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
